c124f6885b4c45eb43d5f9423677fc8ed43c14a7
Rohit Sharma <rs1257933@gmail.com>
2020-05-05 22:11:27 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './ components/MenuComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+      <div className = "App">
+        <Navbar dark color = "dark">
+          <div className = "container">
+            <NavbarBrand href = "/">Amliya da dhaba </NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }

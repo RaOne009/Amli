9f1df77797ee3eb5ad9ae26d60c9dff8a7f960ea
Rohit Sharma <rs1257933@gmail.com>
2020-05-08 17:33:32 +0530

    yo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './ components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       dishes: DISHES
+//     };
+//   }
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  
   render() {
     return (
       <div className = "App">
@@ -12,7 +30,7 @@ class App extends Component {
             <NavbarBrand href = "/">Amliya da dhaba </NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }

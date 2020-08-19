3172b50eefb0352babf045b5b295fc41e58595fa
Rohit Sharma <rs1257933@gmail.com>
2020-08-20 03:04:40 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/MainComponent';
 import './App.css';
-import Menu from './ components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: DISHES
-//     };
-//   }
-
+const store = ConfigureStore();
 class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+
   
   render() {
     return (
-      <div className = "App">
-        <Navbar dark color = "dark">
-          <div className = "container">
-            <NavbarBrand href = "/">Amliya da dhaba </NavbarBrand>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
           </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }

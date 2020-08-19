fd9a4d0f1e68377a15595ae87719e32798d5fefb
Rohit Sharma <rs1257933@gmail.com>
2020-08-20 03:02:48 +0530

    yarn build

---
+++ build/precache-manifest.b3327474fa65c3d68a4319f34ecc61a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "197f18dfa6490f4dbd4c",
+    "url": "/static/css/main.5d0bcd48.chunk.css"
+  },
+  {
+    "revision": "197f18dfa6490f4dbd4c",
+    "url": "/static/js/main.197f18df.chunk.js"
+  },
+  {
+    "revision": "2356e7e0abaf0948d0f8",
+    "url": "/static/css/1.0a08ca50.chunk.css"
+  },
+  {
+    "revision": "2356e7e0abaf0948d0f8",
+    "url": "/static/js/1.2356e7e0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0843094dd03c479df8c94c72ad91caf1",
+    "url": "/index.html"
+  }
+];
